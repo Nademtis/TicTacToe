72eb4dbf9273503d45db81a261475085caef84c3
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2024-02-26 13:15:06 +0100

    added board click functionality

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ window.addEventListener("load", start())
 
 function start(){
     console.log("js");
+    makeBoardClickable();
+}
+
+function makeBoardClickable(){
+    document.querySelector("#board")
+    .addEventListener("click", boardClicked)
+}
+
+function boardClicked(event){
+    console.log('board clicked');
 }
